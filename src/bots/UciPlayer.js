be48915a4aaa9f7fdb09c74282d1da77a18a4aa4
Paul Hutchinson <paul@lexiphanic.co.uk>
2018-05-15 21:38:30 +0100

    Add support for pondering

--- src/bots/UciPlayer.js
+++ src/bots/UciPlayer.js
@@ -1,6 +1,5 @@
 const UciEngine = require('node-uci').Engine;
 
-
 /**
  * Use a UciEngine to decide the best move, if any.
  */
@@ -8,6 +7,8 @@ class UciPlayer {
 
   constructor(path) {
     this.bootEngine(path);
+    this.ponderMoves = null;
+    this.ponderer = null;
   }
 
   async bootEngine(path) {
@@ -18,7 +19,8 @@ class UciPlayer {
   }
 
   async getNextMove(event) {
-    await this.engine.position('startpos', event.movesArray());
+    let moves = event.movesArray();
+    let result;
     var options = {};
     if (event.wtime) {
       options.wtime = event.wtime;
@@ -32,7 +34,39 @@ class UciPlayer {
     if (event.binc) {
       options.wtime = event.wtime;
     }
-    return await this.engine.go(options);
+
+    if (this.ponderMoves !== null && moves.join("-") === this.ponderMoves.join("-")) {
+      console.log('ponder hit');
+      await this.engine.ponderhit();
+      result = await this.ponderer;
+      this.ponderer = null;
+    } else {
+      if (this.ponderMoves !== null) {
+      //  console.log('ponder fail', this.ponderMoves, moves);
+        await this.engine.write('stop');
+      }
+      await this.engine.position('startpos', moves);
+      result = await this.engine.go(options);
+    }
+    this.ponderMoves = null;
+
+    if (result.ponder) {
+      this.ponderMoves = moves;
+      this.ponderMoves.push(result.bestmove);
+      this.ponderMoves.push(result.ponder);
+      this.ponder(moves, options);
+    }
+
+    result.info = null;
+    return result;
+  }
+
+  async ponder(moves, options)
+  {
+      console.log('pondering ', moves);
+      await this.engine.position('startpos', moves);
+      options.ponder = true;
+      this.ponderer = this.engine.go(options);
   }
 
   getReply(chat) {
